0c722cb8372109e103f31bae34cb9f20f0f01328
Alexis Vincent <mail@alexisvincent.io>
2017-07-26 21:13:54 +0200

    Added cluster config service

--- services/inject-services.sh
+++ services/inject-services.sh
@@ -1,8 +1,11 @@
 sudo cp ./dnsmasq.service /etc/systemd/system/dnsmasq.service
 sudo cp ./matchbox.service /etc/systemd/system/matchbox.service
+sudo cp ./cluster-config.service /etc/systemd/system/cluster-config.service
 
 sudo systemctl daemon-reload
 sudo systemctl stop dnsmasq
 sudo systemctl stop matchbox
+sudo systemctl stop cluster-config
 sudo systemctl start dnsmasq
 sudo systemctl start matchbox
+sudo systemctl start cluster-config

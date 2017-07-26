34692795f79e64c6c7b36e2d0a709ae585ca002f
Alexis Vincent <mail@alexisvincent.io>
2017-07-26 16:30:03 +0200

    Compiling in variables working

--- cli/index.js
+++ cli/index.js
@@ -9,16 +9,17 @@ try {
 
   const profile_gateway = fs.readFileSync('../matchbox/profiles/gateway.json', 'utf8');
   const profile_gateway_gen = Mustache.render(profile_gateway, variables)
-  fs.writeFileSync(`../matchbox/profiles/gateway.gen.json`, rendered)
+  fs.writeFileSync(`../matchbox/profiles/gateway.gen.json`, profile_gateway_gen)
 
   const gateway_ignition_raw = fs.readFileSync('../matchbox/ignition/gateway.yaml', 'utf8')
   const gateway_ignition_with_globals = Mustache.render(gateway_ignition_raw, variables);
   fs.writeFileSync('../matchbox/ignition/gateway.gen.yaml', gateway_ignition_with_globals)
   
   const gateway_ignition = yaml.safeLoad(gateway_ignition_with_globals);
+  const profile_variables = variables.network.bootstrapper
   gateway_ignition.systemd.units.forEach(function(unit) {
     Mustache.tags = ["{{", "}}"]
-     const unit_with_profile_variables = Mustache.render(unit.contents, variables);
+     const unit_with_profile_variables = Mustache.render(unit.contents, profile_variables);
      fs.writeFileSync(`../services/${unit.name}`, unit_with_profile_variables)
   }) 
 

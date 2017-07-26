84d069f099562a1ca39b16e9b17e3833ccaa2a17
Alexis Vincent <mail@alexisvincent.io>
2017-07-26 11:54:21 +0200

    Auto generate services and added pynetkey service

--- matchbox/etc/generate-tls.sh
+++ matchbox/etc/generate-tls.sh
@@ -1,3 +1,3 @@
 # DNS or IP Subject Alt Names where matchbox runs
-export SAN=DNS.1:matchbox.example.com,IP.1:10.10.0.1
+export SAN=DNS.1:matchbox.cluster,IP.1:10.10.0.1
 cp ca.crt server.crt server.key ../../../steve-cluster/matchbox/etc

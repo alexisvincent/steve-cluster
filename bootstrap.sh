c111f6205cb719b832bf6bf92518a0b90b719fb4
Alexis Vincent <mail@alexisvincent.io>
2017-07-30 18:16:30 +0200

    Write to writable file system

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz > /usr/local/bin/node
+mkdir -p /opt/bin
+curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz > /opt/bin/node
 sudo git clone https://github.com/alexisvincent/steve-cluster-config /etc/cluster-config
 cd /etc/cluster-config
 node steve.js bootstrap

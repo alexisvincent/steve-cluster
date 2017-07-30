487cd83300074d2712d9643ab936762394a34c30
Alexis Vincent <mail@alexisvincent.io>
2017-07-30 18:17:49 +0200

    Fixed permission issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
-mkdir -p /opt/bin
-curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz > /opt/bin/node
+sudo mkdir -p /opt/bin
+sudo curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz > /opt/bin/node
 sudo git clone https://github.com/alexisvincent/steve-cluster-config /etc/cluster-config
 cd /etc/cluster-config
 node steve.js bootstrap

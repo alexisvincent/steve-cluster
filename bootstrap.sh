768e855cd2b39d18e192962eea590b299edcdec9
Alexis Vincent <mail@alexisvincent.io>
2017-07-31 09:25:20 +0200

    New node lib

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo mkdir -p /opt
 sudo chown core /opt
 mkdir -p /opt/bin
-curl -o /opt/bin/node https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz
+curl -o /opt/bin/node https://github.com/zeit/pkg-fetch/releases/download/v2.3/uploaded-v2.3-node-v8.0.0-linux-x86
 git clone https://github.com/alexisvincent/steve-cluster-config /opt/cluster-config
-cd /etc/cluster-config
+cd /opt/cluster-config
 node steve.js bootstrap

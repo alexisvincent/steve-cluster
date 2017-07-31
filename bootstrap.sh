d34627e03bcb991350ffa682e4ec62ccb455ef7e
Alexis Vincent <mail@alexisvincent.io>
2017-07-31 09:02:48 +0200

    Fixed curl permission error

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /opt/bin
-sudo curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz > /opt/bin/node
+sudo curl -o /opt/bin/node https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x86.tar.xz
 sudo git clone https://github.com/alexisvincent/steve-cluster-config /etc/cluster-config
 cd /etc/cluster-config
 node steve.js bootstrap

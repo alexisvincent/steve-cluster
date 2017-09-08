1788ae21c3c7b70fe1a7148c9021822e4216c344
Alexis Vincent <mail@alexisvincent.io>
2017-09-08 09:51:53 +0200

    Ignition file

--- steve.js
+++ steve.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (__dirname){
 const yaml = require('js-yaml');
 const fs   = require('fs');
 const vorpal = require('vorpal')();
@@ -7,7 +8,6 @@ const Mustache = require('mustache')
 const { exec } = require('child_process');
 Mustache.tags = ['{|', '|}']
 
-process.chdir(__dirname)
 
 const variables = yaml.safeLoad(fs.readFileSync('./variables.yaml', 'utf8'));
 
@@ -22,12 +22,13 @@ vorpal
     // ignition/gateway.yaml -> ignition/gateway.gen.yaml
     const gateway_ignition = fs.readFileSync('matchbox/ignition/gateway.yaml', 'utf8')
     fs.writeFileSync('matchbox/ignition/gateway.gen.yaml', Mustache.render(gateway_ignition, variables))
+    this.log(__root)
     
     // bootstrapper service files
     variables.network.gateway = variables.network.bootstrapper
     const gateway_ignition_bootstrapper = yaml.safeLoad(Mustache.render(gateway_ignition, variables));
     gateway_ignition_bootstrapper.systemd.units
-      .filter(unit => ['matchbox.service', 'dnsmasq.service', 'cluster-config.service'].includes(unit.name))
+      .filter(unit => ['matchbox.service', 'dnsmasq.service'].includes(unit.name))
       .forEach((unit) => fs.writeFileSync(`bootstrapper/services/${unit.name}`, unit.contents)) 
     gateway_ignition_bootstrapper.networkd.units.forEach((unit) => fs.writeFileSync(`bootstrapper/network/${unit.name}`, unit.contents)) 
 
@@ -65,47 +66,14 @@ vorpal
 vorpal
   .command('reload network', 'inject and reload networkd config')
   .action(function(args, cb) {
-    this.log('injecting network services...')
-    exec(`
-    sudo cp bootstrapper/network/gateway.network /etc/systemd/network/gateway.network
 
-    sudo systemctl daemon-reload
-    sudo systemctl restart systemd-networkd
-    `, (err, stdout, stderr) => {
-      this.log(stdout)
-      this.log(stderr)
-      this.log('done')
-      cb();
-    })
-
-  });
-
-vorpal
-  .command('pull assets <host>', 'download coreos images from remote computer')
-  .action(function(args, cb) {
-    exec(`
-    scp -r core@${args.host}:/opt/cluster-config/matchbox/assets /opt/cluster-config/matchbox
-    `,   (err, stdout, stdin) => {
-      this.log(stdout)
-      this.log(stderr)
-    })
-  });
-
-vorpal
-  .command('push assets <host>', 'download coreos images from remote computer')
-  .action(function(args, cb) {
-    exec(`
-    scp -r ./matchbox/assets core@${args.host}:/opt/cluster-config/matchbox
-    `,   (err, stdout, stderr) => {
-      this.log(stdout)
-      this.log(stderr)
-    })
   });
 
 vorpal.parse(process.argv);
 vorpal
   .delimiter('steve >')
   .show();
+}).call(this,"/Users/alexisvincent/Code/steve-cluster/cli")
 },{"child_process":undefined,"fs":undefined,"js-yaml":330,"mustache":363,"path":undefined,"vorpal":390}],2:[function(require,module,exports){
 'use strict';
 var ESC = '\u001b[';

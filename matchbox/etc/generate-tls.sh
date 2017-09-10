8598448a140cd150ce560222a05d7cd77d370127
Alexis Vincent <mail@alexisvincent.io>
2017-09-10 18:29:52 +0200

    Better tls management

--- matchbox/etc/generate-tls.sh
+++ matchbox/etc/generate-tls.sh
@@ -1,3 +1,5 @@
 # DNS or IP Subject Alt Names where matchbox runs
-export SAN=DNS.1:matchbox.cluster,IP.1:10.10.0.1
-cp ca.crt server.crt server.key ../../../steve-cluster/matchbox/etc
+export SAN=DNS.1:localhost,IP.1:10.10.0.1
+cd tls
+curl https://raw.githubusercontent.com/coreos/matchbox/master/scripts/tls/openssl.conf -o openssl.conf
+curl https://raw.githubusercontent.com/coreos/matchbox/master/scripts/tls/cert-gen | bash

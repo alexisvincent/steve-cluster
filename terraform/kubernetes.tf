5d1e1eb4566e9cfdc4ae659fa673e0ffc049b811
Alexis Vincent <mail@alexisvincent.io>
2017-09-28 15:56:37 +0200

    Added new variables

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -2,12 +2,15 @@
 module "kubernetes" {
   source = "git::https://github.com/alexisvincent/typhoon//bare-metal/container-linux/kubernetes"
 
-  matchbox_http_endpoint = "${var.matchbox_http_endpoint}"
-  ssh_authorized_key     = "${var.ssh_authorized_key}"
+  matchbox_http_endpoint  = "${var.matchbox_http_endpoint}"
 
-  cluster_name            = "${var.cluster_name}"
   container_linux_channel = "${var.container_linux_channel}"
   container_linux_version = "${var.container_linux_version}"
+  
+  cluster_name            = "${var.cluster_name}"
+  k8s_domain_name         = "${var.k8s_domain_name}"
+
+  ssh_authorized_key      = "${var.ssh_authorized_key}"
 
   # Machines
   controller_names   = "${var.controller_names}"
@@ -18,12 +21,10 @@ module "kubernetes" {
   worker_domains     = "${var.worker_domains}"
 
   # bootkube assets
-  k8s_domain_name = "${var.k8s_domain_name}"
   asset_dir       = "${var.asset_dir}"
 
   # Optional
   cached_install                = "${var.cached_install}"
   install_disk                  = "${var.install_disk}"
   container_linux_oem           = "${var.container_linux_oem}"
-  experimental_self_hosted_etcd = "${var.experimental_self_hosted_etcd}"
 }

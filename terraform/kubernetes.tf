95ae2d231f94daf0921ce4d37d66466cc371f746
gveeden <gveeden@gmail.com>
2018-04-06 13:04:59 +0000

    Bumped kubernetes to v1.10.0

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,6 +1,6 @@
 // Self-hosted Kubernetes cluster
 module "kubernetes" {
-  source = "git::https://github.com/poseidon/typhoon//bare-metal/container-linux/kubernetes?ref=v1.9.6"
+  source = "git::https://github.com/poseidon/typhoon//bare-metal/container-linux/kubernetes?ref=v1.10.0"
 
 	providers = {
 		local = "local.default"

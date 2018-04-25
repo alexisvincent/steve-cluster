f5576ed8b306b99ebf710eca518c4a8788aca12b
gveeden <gveeden@gmail.com>
2018-04-25 10:30:22 +0000

    Updated kubectl update script to allow for root

--- setup-kubectl.sh
+++ setup-kubectl.sh
@@ -3,7 +3,7 @@ rm /home/core/.bashrc
 cp ./bashrc /home/core/.bashrc
 
 # Install kubectl
-rm ~/.bin/kubectl
+rm /home/core/.bin/kubectl
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kubectl
 mkdir -p /home/core/.bin
 mv ./kubectl /home/core/.bin/kubectl
@@ -11,7 +11,7 @@ chown core /home/core/.bin/kubectl
 chmod +x /home/core/.bin/kubectl
 
 # Install Terraform
-rm ~/.bin/terraform
+rm home/core/.bin/terraform
 wget -O /home/core/.bin/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip -p /home/core/.bin/terraform.zip > /home/core/.bin/terraform
 rm /home/core/.bin/terraform.zip

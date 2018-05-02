d6b62466b6129a424adb95127094ff47646c833a
gveeden <gveeden@gmail.com>
2018-05-02 16:03:24 +0000

    Added helm and StorageOS

--- setup-kubectl.sh
+++ setup-kubectl.sh
@@ -11,8 +11,23 @@ chown core /home/core/.bin/kubectl
 chmod +x /home/core/.bin/kubectl
 
 # Install Terraform
-rm home/core/.bin/terraform
+rm /home/core/.bin/terraform
 wget -O /home/core/.bin/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip -p /home/core/.bin/terraform.zip > /home/core/.bin/terraform
 rm /home/core/.bin/terraform.zip
 chmod +x /home/core/.bin/terraform
+
+# Install Helm
+rm /home/core/.bin/helm
+wget -O /home/core/.bin/helm.tar.gz wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.0-linux-amd64.tar.gz
+tar -zxvf /home/core/.bin/helm.tar.gz
+rm /home/core/.bin/helm.tar.gz
+mv /home/core/.bin/linux-amd64/helm /home/core/.bin/helm
+rm -r /home/core/.bin/linux-amd64
+chmod +x /home/core/.bin/helm
+helm init 
+
+# Install StorageOS
+rm /home/core/.bin/storageos
+curl -sSLo /home/core/.bin/storageos https://github.com/storageos/go-cli/releases/download/0.10.0/storageos_linux_amd64 
+chmod +x /home/core/.bin/storageos
